1a1ea9bf90425f9bf054d9bc516e7fd9a462ec48
Timoty <timoty.granziero@gmail.com>
2019-05-24 17:15:16 +0200

    End up base rolling

---
+++ bot.py
@@ -0,0 +1,43 @@
+import time
+
+import telepot
+from telepot.loop import MessageLoop
+
+import dice
+
+bot = telepot.Bot('TOKEN')
+
+
+def handle(msg):
+    content_type, chat_type, chat_id, _, msg_id = telepot.glance(msg, long=True)
+    dice.logging.debug(f'Received message from {msg["from"]["username"]} ({chat_id})')
+
+    if content_type == 'text':
+        roll_expression, roll_desc = dice.parse(msg['text'])
+        if roll_expression:
+            result, steps = dice.roll(roll_expression)
+            response = build_msg(msg['from']['username'], result, steps, roll_desc)
+            bot.sendMessage(chat_id, response, reply_to_message_id=msg_id, parse_mode='markdown')
+
+def build_msg(username, result, steps, roll_desc):
+    final_msg = ''.join([
+        username,
+        f' rolled *{roll_desc}*:\n`',
+    ])
+    for step in steps:
+        final_msg = ''.join([
+            final_msg,
+            str(step),
+            # ' ',
+        ])
+    final_msg = ''.join([
+        final_msg,
+        '=`\n',
+        f'*{result}*'
+    ])
+    return final_msg
+
+MessageLoop(bot, handle).run_as_thread()
+
+while True:
+    time.sleep(1)
--- dice.py
+++ dice.py
@@ -8,15 +8,35 @@ from enum import Enum
 logging.basicConfig(level='DEBUG', format='%(asctime)s [%(levelname)s] : %(message)s')
 
 # bot_token = ''
+normal_roll = r'(?:/r|/roll)\s+(?:(\d*d\d+)|(\d+))(?:([+-])((\d*d\d+)|(\d+)))*'
+multi_roll = r'(?:/r|/roll)\s+\d*d\d+[+-]\d+(?:/[+-]\d+)+'
 
-class token_kind(Enum):
+
+class TokenKind(Enum):
     ROLL=r'\d*d\d'
     NUMBER=r'\d+'
     OPERATOR=r'[+-]'
 
-def roll(text) -> str:
-    "/r 1d20+4"
 
+def parse(text: str) -> (str, str):
+
+    # Multiattack roll match
+    match_multi_roll = re.search(multi_roll, text)
+    if match_multi_roll:
+        value = match_multi_roll.group(0)
+        logging.debug('Matched multi roll: \'' + value + '\', match length: ' + str(len(value)))
+        logging.debug('Roll descriptor: \'' + text[len(value):len(text)].strip() + '\'')
+        return value, text[len(value):len(text)].strip()
+
+    # Normal match
+    match = re.search(normal_roll, text)
+    if not match_multi_roll and match:
+        value = match.group(0)
+        logging.debug('Matched normal roll: \'' + value + '\', match length: ' + str(len(value)))
+        logging.debug('Roll descriptor: \'' + text[len(value):len(text)].strip() + '\'')
+        return value, text[len(value):len(text)].strip()
+
+    return '', False
 
 def tokenize(text: str, prefix=True) -> list:
     # Only if prefix is True, discard the first element (i.e. /roll)
@@ -40,56 +60,87 @@ def tokenize(text: str, prefix=True) -> list:
     return token_list
 
 
-def get_kind(text: str) -> token_kind:
-    for kind in token_kind: # returns token_kind value or False if no matches
+def get_kind(text: str) -> TokenKind:
+    for kind in TokenKind: # returns TokenKind value or False if no matches
         if re.search(kind.value, text):
             return kind
     return False
 
-def compute(tokens: list) -> int:
+def compute(tokens: list) -> (int, list):
     """Expected input for `tokens`:
 
     `[roll/number, operator?, roll/number?, operator?, roll/number? ...]`
 
     Odd number of elements in tokens
     """
-    result = get(tokens[0])
+    result, local_steps, token_kind = get(tokens[0])
+    steps = [get_step(local_steps, token_kind)]
     index = 1
     while index < len(tokens)-1:
-        operator = get(tokens[index])
-        second = get(tokens[index+1])
+        operator, _, _ = get(tokens[index])
+        second, local_steps, token_kind = get(tokens[index+1])
         if operator == '+':
             result += second
+            steps.append('+')
         elif operator == '-':
             result -= second
+            steps.append('-')
+        steps.append(get_step(local_steps, token_kind))
         index += 2
-    return result
+    return result, steps
 
-def get(value):
+def get(value) -> (int, list, TokenKind):
     kind = get_kind(value)
 
     # Dice roll, format r'\d*d\d+'
-    if get_kind(value) == token_kind.ROLL:
+    if get_kind(value) == TokenKind.ROLL:
         split = value.split('d')
         # Normal split, result of dice format r'\d+d\d+' (e.g. '6d6')
         if len(split[0]) > 0:
-            return int(split[0]) * random.randint(1, int(split[1]))
+            result, steps = process_roll(int(split[0]), int(split[1]))
+            return result, steps, TokenKind.ROLL
         # Check for '', result of dice format r'd\d+' (e.g. 'd20')
         else:
-            return random.randint(1, int(split[1]))
+            result, steps = process_roll(1, int(split[1]))
+            return result, steps, TokenKind.ROLL
 
     # Constant
-    if get_kind(value) == token_kind.NUMBER:
-        return int(value)
+    if get_kind(value) == TokenKind.NUMBER:
+        return int(value), [value], TokenKind.NUMBER
 
     # + or -
-    if get_kind(value) == token_kind.OPERATOR:
-        return value
+    if get_kind(value) == TokenKind.OPERATOR:
+        return value, [value], TokenKind.OPERATOR
 
     # get_kind() returned False
     else:
         raise TypeError
 
+def get_step(steps: list, token_kind: TokenKind) -> str:
+    if token_kind == TokenKind.ROLL:
+        result = '('
+        for step in steps:
+            result = ''.join([
+                result,
+                str(step),
+                '+'
+            ])
+        result = result[:-1] # Removes the last '+'
+        return ''.join([result, ')'])
+    return steps[0]
+
+def roll(roll_expr: str) -> (int, list):
+    return compute(tokenize(roll_expr))
+
+
+def process_roll(iterations, value) -> (int, list):
+    result = 0
+    steps = []
+    for i in range(iterations):
+        random_roll = random.randint(1, int(value))
+        result += random_roll
+        steps.append(random_roll)
+    return result, steps
 
 # logging.debug(tokenize("/r 1d20+23+17"))
 # logging.debug(tokenize("/roll 1d20+12"))
@@ -99,8 +150,14 @@ def get(value):
 # logging.debug(get('1d20'))
 # logging.debug(get('4d20'))
 
-dice_roll = tokenize('/roll 1d20+13')
-logging.debug(compute(dice_roll))
+# dice_roll = tokenize('/roll 1d20+13')
+# logging.debug(compute(dice_roll))
 
-dice_roll = tokenize('/roll 1d20-4d4+12')
-logging.debug('roll: ' + str(compute(dice_roll)))
+# dice_roll = tokenize('/roll 1d20-4d4+12')
+# logging.debug('roll: ' + str(compute(dice_roll)))
+# parse('/r 1d20+12-2+4 ancd asdoasid oiasd ')
+# parse('/r 1d20+12/+7/+2 ancd asdoasid oiasd    ')
+
+# logging.debug(roll('/r 1d20+14'))
+
+logging.debug(get_step([1, 4, 5], token_kind=TokenKind.ROLL))
